14dd41d2b8ce83e1750a00e3dc311ba11c37581f
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-06 09:49:47 -0800

    Move train times to show page. Add station links to index page.

--- server.js
+++ server.js
@@ -7,26 +7,12 @@ const BART_API_KEY = process.env.BART_API_KEY;
 
 app.set('view engine', 'ejs');
 
-// Index
-app.get('/', (req, res) => {
-  axios.get(`http://api.bart.gov/api/etd.aspx?cmd=etd&orig=LAFY&key=${BART_API_KEY}&json=y`)
-    .then((response) => {
-      console.log(response.data.root.station[0])
-      res.render('index', {
-        station: response.data.root.station[0]
-      })
-    })
-    .catch((error) => {
-      res.send(error)
-    })
-});
-
 // Show
 app.get('/:station', (req, res) => {
   axios.get(`http://api.bart.gov/api/etd.aspx?cmd=etd&orig=${req.params.station}&key=${BART_API_KEY}&json=y`)
     .then((response) => {
       console.log(response.data.root.station[0])
-      res.render('index', {
+      res.render('show', {
         station: response.data.root.station[0]
       })
     })
@@ -34,6 +20,21 @@ app.get('/:station', (req, res) => {
       res.send(error)
     })
 })
+
+// Index
+app.get('/', (req, res) => {
+  axios.get(`http://api.bart.gov/api/stn.aspx?cmd=stns&key=${BART_API_KEY}&json=y`)
+    .then((response) => {
+      console.log(response.data.root.stations)
+      res.render('index', {
+        stations: response.data.root.stations.station
+      })
+      // res.send(response.data.root.stations.station)
+    })
+    .catch((error) => {
+      res.send(error)
+    })
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
