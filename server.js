a1ee3fb93b419ce38bcd668d5e5d65a8eefa93d1
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-05 22:08:52 -0800

    Set up landing page, ejs

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const axios = require('axios');
 const PORT = process.env.PORT || 4000;
 const BART_API_KEY = process.env.BART_API_KEY;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   axios.get(`http://api.bart.gov/api/etd.aspx?cmd=etd&orig=LAFY&key=${BART_API_KEY}&json=y`)
     .then((response) => {
-      res.send(response.data)
+      console.log(response.data.root.station[0])
+      res.render('index', {
+        station: response.data.root.station[0]
+      })
     })
     .catch((error) => {
       res.send(error)
