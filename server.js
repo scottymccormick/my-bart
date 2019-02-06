fe70a105d24a3bba46dfe4b0262b2e7422369175
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-05 21:28:46 -0800

    Set up .env with API key

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 4000;
+const BART_API_KEY = process.env.BART_API_KEY;
 
 app.get('/', (req, res) => {
-  res.send('My Bart Landing Page')
+  res.send('My Bart Landing Page');
 })
 
 app.listen(PORT, () => {
